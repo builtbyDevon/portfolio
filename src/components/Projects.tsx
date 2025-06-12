f4f0a5dda41d3774d31dfe6bc8e8145d356202c9
Devon Welch <deveidog7@gmail.com>
2025-06-12 17:01:12 -0600

    Remove wrangler, see if its needed

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -54,9 +54,6 @@ const Projects = ({ projects }: ProjectsProps) => {
       <FadeInView delay={1.2}>
         <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 md:gap-8 lg:grid-cols-4">
           {projects.map((project) => {
-            console.log(
-              `Rendering card for: ${project.title}, slug: ${project.slug}, webUrl: ${project.webUrl}`
-            );
             return (
               <ProjectCard
                 key={project.id}
