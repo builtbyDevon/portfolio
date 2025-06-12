670ca36b0812138549349cb750799be4f16fcaac
Devon Welch <deveidog7@gmail.com>
2025-06-12 16:01:38 -0600

    Deploy fix attempt #1

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "highlight-green": "var(--highlight-green)",
+        "highlight-blue": "var(--highlight-blue)",
+      },
+    },
+  },
+  plugins: [],
+};
